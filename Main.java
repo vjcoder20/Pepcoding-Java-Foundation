c66917792edcd48589bdfa89f3aebb377080ab03
Vaibhav Jain <jain.vaibhav020@gmail.com>
2021-06-10 11:56:46 +0530

    I/o learning

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+ class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // System.out.print(n);
+        // System.out.print(n);
+        String s = sc.nextLine();
+        System.out.println(s);
+        sc.close();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Pepcoding Java Foundation
- Code of java from pepcoding
+ Code of java from pepcoding by Summet Sir YouTube Playlist
