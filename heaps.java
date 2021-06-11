477c465bfd22097e1d4def7123239d9a9da4f501
Vaibhav Jain <jain.vaibhav020@gmail.com>
2021-06-11 20:48:37 +0530

    cool

--- heaps.java
+++ heaps.java
@@ -5,7 +5,7 @@ import java.util.PriorityQueue;
 public class heaps {
     public static void main(String[] args) {
         // PriorityQueue<Integer> pq = new PriorityQueue<>(); //print in increasing sorted order
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); //print in decreasing sorted order
+    PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); //print in decreasing sorted order
         int[] ranks = {22,99,3,11,88,4,1};
         for(int val:ranks)
         pq.add(val);
