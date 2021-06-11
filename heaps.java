5ee2323f9bb22666c526958f42e60e43d73b13c3
Vaibhav Jain <jain.vaibhav020@gmail.com>
2021-06-11 14:08:56 +0530

    heaps-priorityqueue

---
+++ heaps.java
@@ -0,0 +1,17 @@
+import java.util.Collection;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class heaps {
+    public static void main(String[] args) {
+        // PriorityQueue<Integer> pq = new PriorityQueue<>(); //print in increasing sorted order
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); //print in decreasing sorted order
+        int[] ranks = {22,99,3,11,88,4,1};
+        for(int val:ranks)
+        pq.add(val);
+        while(pq.size()>0){
+    System.out.println(pq.peek());
+    pq.remove();
+        }
+    }
+}
