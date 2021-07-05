1698254c813f204fb52b5546b283ddb73992abd5
Vaibhav Jain <jain.vaibhav020@gmail.com>
2021-07-05 09:57:18 +0530

    questions updated

--- pattern21.java
+++ pattern21.java
@@ -1,3 +1,12 @@
+/* 
+For n=5                       For n=6
+1 2 3 4 5                      1 2 3 4 5 6
+11 12 13 14 15                13 14 15 16 17 18
+21 22 23 24 25                25 26 27 28 29 30
+16 17 18 19 20                31 32 33 34 35 36
+6 7 8 9 10                    19 20 21 22 23 24
+                              7 8 9 10 11 12
+*/
 import java.util.*;
 public class pattern21 {
     public static void main(String[] args) {
